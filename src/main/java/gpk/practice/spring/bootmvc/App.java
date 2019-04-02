b536265de604599eb57539fabd3dafcebee8f360
Gennady Portenko <gportenko@gmail.com>
2019-04-02 14:00:09 +0400

    Initial

---
+++ src/main/java/gpk/practice/spring/bootmvc/App.java
@@ -0,0 +1,14 @@
+package gpk.practice.spring.bootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@Component
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
