08a1dc9d7d0d7b24c05961f326dbe0476185a57f
Gennady Portenko <gportenko@gmail.com>
2019-04-16 17:33:17 +0400

    Refactored logging out with Spring Security

--- src/main/java/gpk/practice/spring/bootmvc/controller/LoginController.java
+++ src/main/java/gpk/practice/spring/bootmvc/controller/LoginController.java
@@ -3,6 +3,9 @@ package gpk.practice.spring.bootmvc.controller;
 import gpk.practice.spring.bootmvc.model.User;
 import gpk.practice.spring.bootmvc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -97,10 +101,12 @@ public class LoginController {
     }
 
     @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public ModelAndView logOut(HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        session.invalidate();
-        return new ModelAndView("redirect:/");
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 
 }
