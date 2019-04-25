ad912c5e9c05e4b5c66c9f1bbfbc0b8159ee3563
Gennady Portenko <gportenko@gmail.com>
2019-04-25 10:00:22 +0400

    Added simple ajax call processing

--- src/main/java/gpk/practice/spring/bootmvc/controller/MainController.java
+++ src/main/java/gpk/practice/spring/bootmvc/controller/MainController.java
@@ -1,12 +1,18 @@
 package gpk.practice.spring.bootmvc.controller;
 
+import gpk.practice.spring.bootmvc.dto.UserDto;
 import gpk.practice.spring.bootmvc.service.SecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
@@ -18,6 +24,15 @@ public class MainController {
         ModelAndView modelAndView = new ModelAndView("index");
         modelMap.put("username", securityService.getCurrentUserName());
         return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody
+    UserDto add(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setUsername( request.getParameter("firstName") );
+        userDto.setEmail( request.getParameter("email") );
+        return userDto;
     }
 
 }
