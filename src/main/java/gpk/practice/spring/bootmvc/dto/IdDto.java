5f34fb9af63c2fef5f335b939144b55eaea17b3d
Gennady Portenko <gportenko@gmail.com>
2019-07-11 17:08:43 +0400

    refactored fixed long polling logic replying to messages works again

--- src/main/java/gpk/practice/spring/bootmvc/dto/IdDto.java
+++ src/main/java/gpk/practice/spring/bootmvc/dto/IdDto.java
@@ -3,18 +3,11 @@ package gpk.practice.spring.bootmvc.dto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.time.Instant;
-import java.util.List;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
-public class MessageDto {
-    long id;
-    Instant datetime;
-    String text;
-    String username;
-    List<MessageDto> MessagesToReply;
+public class IdDto {
+    Long id;
 }
