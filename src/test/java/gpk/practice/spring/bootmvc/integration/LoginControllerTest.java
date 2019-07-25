bd64eed4bab7382562dfbf2a5e2e1dbbcdfbb40b
Gennady Portenko <gportenko@gmail.com>
2019-07-25 17:09:36 +0400

    fixed logging in test

--- src/test/java/gpk/practice/spring/bootmvc/integration/LoginControllerTest.java
+++ src/test/java/gpk/practice/spring/bootmvc/integration/LoginControllerTest.java
@@ -1,35 +1,63 @@
 package gpk.practice.spring.bootmvc.integration;
 
+import gpk.practice.spring.bootmvc.App;
+import gpk.practice.spring.bootmvc.configuration.AppConfig;
+import gpk.practice.spring.bootmvc.configuration.DBTestProfileConfig;
+import gpk.practice.spring.bootmvc.configuration.SecurityConfig;
+import gpk.practice.spring.bootmvc.controller.LoginController;
+import gpk.practice.spring.bootmvc.dto.UserDto;
+import gpk.practice.spring.bootmvc.model.User;
+import gpk.practice.spring.bootmvc.service.UserService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = {
+        App.class,
+        AppConfig.class,
+        SecurityConfig.class,
+        DBTestProfileConfig.class})
+@ActiveProfiles("test")
 public class LoginControllerTest {
     @Autowired
     private WebApplicationContext context;
+    @Autowired
+    private UserService userService;
 
     private MockMvc mockMvc;
 
-    private boolean mockMvcInitialized = false;
+    final String USERNAME = "George";
+    final String PASSWORD= "password";
+    final String EMAIL = "george@email.com";
+
     @Before
     public void init() {
-        if (!mockMvcInitialized) {
-            RestAssuredMockMvc.webAppContextSetup(context);
-            mockMvcInitialized = true;
+        RestAssuredMockMvc.webAppContextSetup(context);
 
-            // mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        }
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @After
+    public void clean() {
+        userService.deleteAll();
     }
 
     @Test
@@ -49,38 +77,37 @@ public class LoginControllerTest {
     }
 
     @Test
-    public void givenContext_whenPostRegister_thenReturnsCode200 () throws Exception {
-        /*
-        UserDto userDto = new UserDto("test_user", "example@mail.com", "password");
-        given()
-                .contentType("application/json")
-                .spec()
-                .when().post("/register", userDto)
-                .then().statusCode(200);
-                */
-        // mockMvc.perform(get("/"))
-                // .andExpect(status().isOk());
-    }
-
-    @Test
     public void givenContext_whenGetIndexNotAuthenticated_thenReturnsCode302 () {
          given()
-                .when()
-                .get("/")
+                .when().get("/")
                 .then().statusCode(302);
     }
 
     @Test
-    @WithMockUser(username="g", password="g", roles="USER")
-    public void givenContext_whenGetIndexAuthenticated_thenReturnsCode200 () {
+    @WithMockUser(username=USERNAME, password=PASSWORD, roles="USER")
+    public void registrationAndLoggingInTest() throws Exception{
+
+        userService.registerNewUserAccount(new User(EMAIL, USERNAME, PASSWORD));
+
+        /*
+        // TODO : instead of user creation via UserService
+        mockMvc.perform(post("/register")
+                        .param("name", USERNAME)
+                        .param("email", EMAIL)
+                        .param("password", PASSWORD))
+                        // .andExpect(MockMvcResultMatchers.redirectedUrl("/index"));
+                        .andExpect(status().isOk());
+                        */
+
         given()
-                .param("username", "g")
-                .param("password", "g")
+                .param("username", USERNAME)
+                .param("password", PASSWORD)
                 .when().post("/login")
                 .then().statusCode(302);
+
         given()
                 .when().get("/")
-                .then().statusCode(200);
+                .then().statusCode(200);  //  logged in : 200 instead of 302
     }
 
 }
